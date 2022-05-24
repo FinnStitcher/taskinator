93dfdf3f2173cdec18d82a3fb32aab3168d4f04d
Finn Stitcher <foundfootage@protonmail.com>
2022-05-24 16:18:01 -0700

    refactored functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,7 +2,22 @@ var formEl = document.querySelector("#task-form");
 
 var toDoEl = document.querySelector("#to-do");
 
-var createTaskHandler = function(event) {
+var createTaskEl = function(taskDataObj) {
+    var listItemEl = document.createElement("li");
+    listItemEl.className = "task-item";
+
+    var taskInfoEl = document.createElement("div");
+    taskInfoEl.className = "task-info";
+    taskInfoEl.innerHTML = `
+        <h3 class="task-name">${taskDataObj.name}</h3>
+        <span class="task-type">${taskDataObj.type}</span>
+    `;
+    
+    listItemEl.appendChild(taskInfoEl);
+    toDoEl.appendChild(listItemEl);    
+};
+
+var taskFormHandler = function(event) {
     event.preventDefault();
     // this prevents the page from refreshing when we click the submit button
     // that refresh is part of the default behavior of a submit event
@@ -11,23 +26,17 @@ var createTaskHandler = function(event) {
     // selecting the first DOM object with the tag <input> and the name "task-name"
     // more generally the square brackets are used to select an element based on one of its attributes
     // its kind of equivalent to pulling specific items from an array
-    var taskTypeInput = document.querySelector("select[name='task-type'").value;
+    var taskTypeInput = document.querySelector("select[name='task-type']").value;
 
-    var listItemEl = document.createElement("li");
-    listItemEl.className = "task-item";
+    var taskDataObj = {
+        name: taskNameInput,
+        type: taskTypeInput
+    };
 
-    var taskInfoEl = document.createElement("div");
-    taskInfoEl.className = "task-info";
-    taskInfoEl.innerHTML = `
-        <h3 class="task-name">${taskNameInput}</h3>
-        <span class="task-type">${taskTypeInput}</span>
-    `;
-    
-    listItemEl.appendChild(taskInfoEl);
-    toDoEl.appendChild(listItemEl);
+    createTaskEl(taskDataObj);
 };
 
-formEl.addEventListener("submit", createTaskHandler);
+formEl.addEventListener("submit", taskFormHandler);
 // no parentheses after the createTaskHandler call because that would call it as the page loads
 // also it doesn't respond to the button if i do that. unsure why THAT is
 
