f57912b21ef01227afaf70f4424b67490955ab48
Finn Stitcher <foundfootage@protonmail.com>
2022-05-25 01:30:56 -0700

    task controls added dynamically

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,16 @@
 var formEl = document.querySelector("#task-form");
 
 var toDoEl = document.querySelector("#to-do");
+var inProgressEl = document.querySelector("#in-progress");
+var completedEl = document.querySelector("#completed");
 
-var createTaskEl = function(taskDataObj) {
+var taskIdCounter = 0;
+
+function createTaskEl(taskDataObj) {
     var listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
+    listItemEl.setAttribute("data-task-id", taskIdCounter);
+    // create <li> element, give it a class and an id using a custom attribute
 
     var taskInfoEl = document.createElement("div");
     taskInfoEl.className = "task-info";
@@ -12,9 +18,63 @@ var createTaskEl = function(taskDataObj) {
         <h3 class="task-name">${taskDataObj.name}</h3>
         <span class="task-type">${taskDataObj.type}</span>
     `;
+    // create a <div>, give it a class and some innerHTML
+
+    var taskActionsEl = createTaskActions(taskIdCounter);
+    // declare the variable taskActionsEl, and assign it the return value of createTaskActions()
+    // we give createTaskActions() the current counter number as its parameter of taskId
     
     listItemEl.appendChild(taskInfoEl);
-    toDoEl.appendChild(listItemEl);    
+    // append the <div> with the information about the task to the <li>
+    listItemEl.appendChild(taskActionsEl);
+    // append the <div> with the task controls to the <li>
+    toDoEl.appendChild(listItemEl);
+    // append the <li> to the <ul>
+
+    taskIdCounter++;
+};
+
+function createTaskActions(taskId) {
+    var actionContainerEl = document.createElement("div");
+    actionContainerEl.className = "task-actions";
+    // create container for buttons and stuff
+
+    var editButtonEl = document.createElement("button");
+    editButtonEl.textContent = "Edit";
+    editButtonEl.className = "btn edit-btn";
+    editButtonEl.setAttribute("data-task-id", taskId);
+    // make edit button
+
+    var deleteButtonEl = document.createElement("button");
+    deleteButtonEl.textContent = "Delete";
+    deleteButtonEl.className = "btn delete-btn";
+    deleteButtonEl.setAttribute("data-tak-id", taskId);
+    // make delete button
+
+    var statusSelectEl = document.createElement("select");
+    statusSelectEl.className = "select-status";
+    statusSelectEl.setAttribute("name", "status-change");
+    statusSelectEl.setAttribute("data-task-id", taskId);
+    // make dropdown that will allow status selection (e.g. to-do/in-progress/complete)
+
+    var statusChoices = ["To Do", "In Progress", "Completed"];
+    for (var i = 0; i < statusChoices.length; i++) {
+        var statusOptionEl = document.createElement("option");
+        statusOptionEl.textContent = statusChoices[i];
+        statusOptionEl.setAttribute("value", statusChoices[i]);
+
+        statusSelectEl.appendChild(statusOptionEl);
+    };
+    // loop through the list of possible task statuses (statusChoices)
+    // for each one, create an <option> element, fill it with the value of the current statusChoice, and set its value to the same
+    // then append the new <option> element to the <select> (dropdown) element we made outside the loop
+
+    actionContainerEl.appendChild(editButtonEl);
+    actionContainerEl.appendChild(deleteButtonEl);
+    actionContainerEl.appendChild(statusSelectEl);
+    // append the edit button, delete button, and dropdown to the container, in that order
+
+    return actionContainerEl;
 };
 
 var taskFormHandler = function(event) {
@@ -43,8 +103,7 @@ var taskFormHandler = function(event) {
 };
 
 formEl.addEventListener("submit", taskFormHandler);
-// no parentheses after the createTaskHandler call because that would call it as the page loads
-// also it doesn't respond to the button if i do that. unsure why THAT is
+// no parentheses after the createTaskHandler call because that would call it as the page loads, rather than giving it to the event listener as a parameter
 
 // event type submit means this event listener will watch for
     // 1. a button with the type "submit" being clicked
